8db8e4c7ce8f6c282fac09ad5edc949f77b8f1ad
BoukermaKhaled <samedxkhaled@gmail.com>
2025-10-07 22:09:50 +0100

    project & styles setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h3 className="text-tgreen font-light">errrr</h3>
+    </>
+  )
+}
+
+export default App
