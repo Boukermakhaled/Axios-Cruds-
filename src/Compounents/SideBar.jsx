6e1cb898ad996f4070b21e52a907c4cee35964a4
BoukermaKhaled <samedxkhaled@gmail.com>
2025-10-08 19:13:05 +0100

    side bar is ready

--- src/Compounents/SideBar.jsx
+++ src/Compounents/SideBar.jsx
@@ -1,17 +1,22 @@
-import {EllipsisVertical} from 'lucide-react'
+import {EllipsisVertical, ShoppingCart} from 'lucide-react'
+import { NavLink } from 'react-router-dom'
 export default function SideBar(){
     return(
-        <div className="side">
-            <div className="profile bg-light-white p-4 w-fit rounded-2xl">
-                <div className="informations flex gap-5">
-                    <img src="photos/Abderrahmane.jpg" alt="" className="w-14 rounded-full" />
-                    <div className="info">
+        <div className="side flex flex-col gap-20 w-fit px-3 py-6 shadow-2xl rounded-2xl h-screen">
+            <div className="profile bg-light-white p-2 w-fit border border-gray-300 rounded-2xl">
+                <div className="informations flex gap-3 lg:gap-5 justify-center items-center">
+                    <img src="photos/Abderrahmane.jpg" alt="" className="w-12 rounded-full" />
+                    <div className="info text-xs w-full lg:text-sm text-left ">
                         <h3 className="font-bold">Abderrahmane Dev</h3>
                         <h3 className="text-gray-400">Front end developer</h3>
                     </div>
-                    <EllipsisVertical />
+                    <EllipsisVertical size={40}/>
                 </div>
             </div>
+
+             <div className="navigation">
+                   <NavLink to={'/'}className={'flex gap-3 bg-light-white border border-gray-300 rounded-2xl p-3 w-full'}><ShoppingCart /> products</NavLink>
+                </div>
         </div>
     )
 }
